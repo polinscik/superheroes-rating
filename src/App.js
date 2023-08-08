f1c45f663f98900352fa1a8a14281799c76d3eb2
Polina <polinscik@gmail.com>
2023-08-08 17:24:08 +0300

    after review

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ function App() {
     <div className="App">
       <h1 className="App-header">Оцени любимых супергероев!</h1>
       <div id="card-container">
-        {JsonData.map((hero) => 
+        {JsonData.map((hero) => (
           <Card
+            key={hero.name}
             name={hero.name}
             universe={hero.universe}
             alterego={hero.alterego}
             occupation={hero.occupation}
             friends={hero.friends}
             superpowers={hero.superpowers}
-            url={hero.url}></Card>,
-        )}
+            url={hero.url}></Card>
+        ))}
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,25 +2,25 @@ import "../styles/card.css"
 
 import blankStar from '../blank-star.svg';
 
-function Card(props) {
+function Card({name, universe, alterego, occupation, friends, superpowers, url, index}) {
   return (
     <div className="card">
-      <h2 className="hero-name">{props.name}</h2>
-      <div>Вселенная: {props.universe}</div>
-      <div>Альтер-эго: {props.alterego}</div>
-      <div>Род деятельности: {props.occupation}</div>
-      <div>Друзья: {props.friends}</div>
-      <div>Суперсила: {props.superpowers}</div>
-      <img src={props.url} className="hero-img"></img>
-      <button className="btn" id="btn${index}">
+      <h2 className="hero-name">{name}</h2>
+      <div>Вселенная: {universe}</div>
+      <div>Альтер-эго: {alterego}</div>
+      <div>Род деятельности: {occupation}</div>
+      <div>Друзья: {friends}</div>
+      <div>Суперсила: {superpowers}</div>
+      <img src={url} className="hero-img"></img>
+      <button className="btn" id={`btn${index}`}>
         Читать подробнее
       </button>
-      <div className="rating" id="${index}">
-        <img value="0" src={blankStar}></img>
-        <img value="1" src={blankStar}></img>
-        <img value="2" src={blankStar}></img>
-        <img value="3" src={blankStar}></img>
-        <img value="4" src={blankStar}></img>
+      <div className="rating" id={index}>
+        <img value="0" src={blankStar} alt=''></img>
+        <img value="1" src={blankStar} alt=''></img>
+        <img value="2" src={blankStar} alt=''></img>
+        <img value="3" src={blankStar} alt=''></img>
+        <img value="4" src={blankStar} alt=''></img>
       </div>
     </div>
   );
